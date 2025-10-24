1648fd2dd476550925b1850ede5b9316bd251e93
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-24 10:08:29 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { add, diff, multiplytwo } from './utlis/math/math'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const sum =add(2,4)
+  console.log(sum)
+  const biyog = diff(4,2);
+  console.log(biyog)
+  const multi = multiplytwo(5,2);
+  console.log(multi)
 
   return (
     <>
